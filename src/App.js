788428bbe31a2509454c54ba1832149518460033
Your Name <you@example.com>
2022-11-15 12:28:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
+import { Button, Spinner } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button>simple button</button> <br /> <br />
+      <Button variant='warning'>react bootstrap button</Button> <br /> <br />
+      <Spinner animation="grow" variant="dark" />
     </div>
   );
 }
